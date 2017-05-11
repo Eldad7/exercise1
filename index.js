ff0d8cba35ed7424f63fab62708b34c62526830e
Eldad7 <eldad.corem@gmail.com>
2017-05-11 23:44:35 +0300

    Get all messages working

--- index.js
+++ index.js
@@ -19,21 +19,29 @@ app.all('*',
 app.get('/allMessages',
 	(req,res) => {
 		console.log(`get: all messages`);
-		res.status(200).json(messages.getAllMessages());
+		res.status(200).send(messages.getAllMessages());
 	});
 
 
 app.post('/userMessages_id/',
 	(req,res) => {
 		console.log(`get messages of: ${req.body.user_id}`);
-		res.status(200).json(messages.getMessagesById(req.body.user_id));
+		res.status(200).send(messages.getMessagesById(req.body.user_id));
 	});
 
 app.post('/userMessages_name/',
 	(req,res) => {
 		console.log(`get messages of: ${req.body.user_name}`);
-		res.status(200).json(messages.getMessagesByUsername(req.body.user_name));
+		res.status(200).send(messages.getMessagesByUsername(req.body.user_name));
 	});
+
+app.post('/userMessages_date/',
+	(req,res) => {
+		console.log(`get messages of:${req.body.user_id}, ${req.body.user_name}, ${req.body.from_date}, ${req.body.to_date}`);
+		res.status(200).send(messages.getUserMessagesFromDate(req.body.user_id,req.body.user_name, req.body.from_date, req.body.to_date));
+	});
+
+
 
 app.listen(port,
 	() => {
--- privateMessages/index.js
+++ privateMessages/index.js
@@ -21,6 +21,8 @@ class Messages{
 		for (let message of this.messages)
 			if (message.user.id == id)
 				userMessages.push(message);
+		if (userMessages.length==0)
+			return {"error id": '001',"error message": "UserID don't exist"};
 		this.sort(userMessages);
 		return userMessages;
 	}
@@ -35,53 +37,77 @@ class Messages{
 		for (let message of this.messages)
 			if (message.user.name == username)
 				userMessages.push(message);
-		//this.sort(userMessages);
-		return userMessages;
-	}
-
-	getUserMessagesFromDate(username,fromDate,toDate=null){	//toDate - Optional
-		var from = new Date(fromDate);
-		var flag = false;		//flag for if there is a toDate
-		var userMessages = [];
-		if (toDate!=null){
-			flag = true;
-			var to = new Date(toDate);
-		}
-		for (let message of this.messages){
-			if (to){
-				if (message.user.name == username && new Date(message.recieved.date).valueOf() >= from && new Date(message.recieved.date).valueOf() <= to)
-					userMessages.push(message);
-			}
-			else{
-				if (message.user.name == username && new Date(message.recieved.date).valueOf() >= from)
-					userMessages.push(message);
-			}
-		}
+		if (userMessages.length==0)
+			return {"error id": '002',"error message": "Username don't exist"};
 		this.sort(userMessages);
 		return userMessages;
 	}
 
-	getUserMessagesFromDate(id,fromDate,toDate=null){	//toDate - Optional
-		var from = new Date(fromDate);
-		var flag = false;		//flag for if there is a toDate
+	/*
+		* need to check the parameters that could be null - to update flags
+		* the to_flag will be inside the for loop, username and id flags will be checked to decide the conditions
+	*/
+
+	getUserMessagesFromDate(id=null, username=null,fromDate,toDate=null){	//username, toDate - Optional 
+		var from = new Date(fromDate).getTime();
+		var to;
+		var flag_id = false, flag_username = false, flag_to = false;		//flags for parameters that might be null
 		var userMessages = [];
+		if (fromDate==null){
+			return {"error id": '005',"error message": "Wrong parameters - From can't be null"};
+		}
+		if (id!=null)
+			flag_id=true;
+		if (username!=null)
+			flag_username=true;
 		if (toDate!=null){
-			flag = true;
-			var to = new Date(toDate);
+			flag_to = true;
+			to = new Date(toDate).getTime();		
+			if (from>=to)
+				return {"error id": '006',"error message": "Wrong parameters - From date can't be greater than to date"};
 		}
-		for (let message of this.messages){
-			if (to){
-				if (message.user.id == id && new Date(message.recieved.date).valueOf() >= from && new Date(message.recieved.date).valueOf() <= to)
-					userMessages.push(message);
-			}
-			else{
-				if (message.user.id == id && new Date(message.recieved.date).valueOf() >= from)
-					userMessages.push(message);
+		if (flag_username==true){
+			for (let message of this.messages){
+				if (flag_to){
+					if (message.user.id == id && new Date(message.recieved.date).getTime() >= from && new Date(message.recieved.date).getTime() <= to)
+						userMessages.push(message);
+				}
+				else{
+					if (message.user.id == id && new Date(message.recieved.date).getTime() >= from);
+						userMessages.push(message);
+				}
 			}
 		}
+		else if (flag_id){
+			for (let message of this.messages){
+				if (flag_to){
+					if (message.user.id == id && new Date(message.recieved.date).getTime() >= from && new Date(message.recieved.date).getTime() <= to)
+						userMessages.push(message);
+				}
+				else{
+					if (message.user.id == id && new Date(message.recieved.date).getTime() >= from)
+						userMessages.push(message);
+				}
+			}
+		}
+		else{
+			for (let message of this.messages){
+				if (flag_to){
+					if (new Date(message.recieved.date).getTime() >= from && new Date(message.recieved.date).getTime() <= to)
+						userMessages.push(message);
+				}
+				else{
+					if (new Date(message.recieved.date).getTime() >= from)
+						userMessages.push(message);
+				}
+			}
+		}
+		if (userMessages.length==0)
+			return {"error id": '004',"error message": "No messages between chosen dates"};
 		this.sort(userMessages);
 		return userMessages;
 	}
+
 }
 
 module.exports = Messages;
