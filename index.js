7ae160e12154c2fa56b9da098082e96bfbe3aebc
Eldad7 <eldad.corem@gmail.com>
2017-05-11 17:28:51 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+const express = require ('express'),
+	  bodyParser = require('body-parser'),
+	  app = express(),
+	  port = process.env.PORT || 3000,
+	  data = require('./data/music.json');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*app.all('*',
+		(req, res, next) => {
+			console.log("runs for all HTTP verbs first");
+			next();
+		});*/
+
+app.get('/getMusicName/:music_id',
+	(req,res) => {
+		var music_id = req.params.music_id;
+		console.log(`get: ${req.params.music_id}`);
+		res.status(200).json({"music-name":data.name});
+	});
+
+/*app.put('/products/:prod_id',
+	(req,res) => {
+		x`console.log('put: ${req.params.prod_id}');
+		res.json({prod_id: req.params.prod_id});
+	});
+*/
+
+app.post('/savemusic/',
+	(req,res) => {
+		var songs = req.body.songs;
+		console.log(`post: ${req.body.songs}`);
+		res.json({'success': 1});
+	});
+
+app.listen(port,
+	() => {
+			console.log(`listening on port ${port}`);
+	});
